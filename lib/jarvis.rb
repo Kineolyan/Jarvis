589e2db0812e8b299a3787a9f5576a0d5ae93028
Olivier Peyrusse <ope@quartetfs.com>
2015-05-06 13:53:10 +0200

    Initialization of the project with basic skeleton.

---
+++ lib/jarvis.rb
@@ -0,0 +1,4 @@
+ROOT = File.dirname(__FILE__)
+$LOAD_PATH.unshift ROOT unless $LOAD_PATH.include? ROOT
+
+require "jarvis/instance"
---
+++ lib/jarvis/instance.rb
@@ -0,0 +1,13 @@
+module Jarvis
+
+  # The main class of the project starting everything
+  class Instance
+
+    # Start the instance
+    def start
+      puts "[Jarvis]>> Hello Sir"
+    end
+
+  end
+
+end
