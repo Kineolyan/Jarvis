cafc66b03a61c1a8d01c3a96e0c2ac5651d665c2
Kineolyan <kineolyan@gmail.com>
2016-05-10 21:41:22 +0200

    Rename files as guidelines.

--- lib/jarvis.rb
+++
@@ -1,8 +0,0 @@
-ROOT = File.dirname(__FILE__)
-$LOAD_PATH.unshift ROOT unless $LOAD_PATH.include? ROOT
-
-require "jarvis/instance"
-require "jarvis/interface/ios"
-require "jarvis/interface/dialog"
-require "jarvis/parser/interpreter"
-require "jarvis/parser/rule"
